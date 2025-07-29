7af5cd1dcc53fbb3e48c2736652d2a7f48e95aab
hadi.prms <hadi.pourmohammad1383@gmail.com>
2025-07-29 15:56:14 +0330

    connect to mongoDB

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express= require('express')
+require('./db/mongoose')
+const User=require('./models/users')
+const userRouter=require('./routers/user')
+
+const app=express()
+const port= 3000
+
+app.use(express.json())
+app.use(userRouter)
+
+app.listen(port,()=>{
+    console.log('listening on', port)
+})
