32f692dfbf178f2828800cd0d3d2760d34fb1a11
hadi.prms <hadi.pourmohammad1383@gmail.com>
2025-07-30 15:08:07 +0330

    add function for hashing password of user

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 
@@ -39,6 +40,15 @@ const UserSchema = new mongoose.Schema({
         required: true
         }
     }]
+});
+
+UserSchema.pre('save', async function(next) {
+    const user = this
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    next();
 });
 
 UserSchema.methods.generateAuthToken = async function(){
