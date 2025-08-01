96e2870626c0a4eb77c57c6e7c33f5d141e95eb5
hadi.prms <hadi.pourmohammad1383@gmail.com>
2025-08-01 23:47:17 +0330

    hotel basic routes added

--- src/routers/hotel.js
+++ src/routers/hotel.js
@@ -3,18 +3,28 @@ const Hotel = require('../models/hotelSchema')
 
 const router = express.Router()
 
-router.post('/hotel', async(req,res) =>{
-    const hotel = new Hotel(req.body)
-    try{
-        await hotel.save()
-        res.status(201).send(hotel)
-    }catch(error){
-        res.status(400).send(error)
+router.post('/hotel', async (req, res) => {
+    try {
+        const existingHotel = await Hotel.findOne(req.body);
+        if (existingHotel) {
+            return res.status(400).send({ error: 'Hotel with the same data already exists.' });
+        }
+
+        const hotel = new Hotel(req.body);
+        await hotel.save();
+        res.status(201).send(hotel);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.get('/hotels' , async (req , res) =>{
+    try {
+        const hotels = await Hotel.find({});
+        res.send(hotels);
+    } catch (error) {
+        res.status(500).send({ error: 'Failed to fetch hotels.' });
     }
 })
-
-router.get('/fromHotel', (req, res) => {
-    res.send('From hotel.js');
-});
 
 module.exports=router;
