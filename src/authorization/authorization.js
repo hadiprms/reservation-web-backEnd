15595140fea88866fcee6e1c47c59289ba8d1923
hadi.prms <hadi.pourmohammad1383@gmail.com>
2025-08-03 13:43:57 +0330

    handle checking role in authorization

--- src/authorization/authorization.js
+++ src/authorization/authorization.js
@@ -1,21 +1,25 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userSchema');
 
-const auth = async(req,res,next) =>{
-    try{
-        const token = req.header('Authorization').replace('Bearer ','')
-        const decode = jwt.verify(token , 'this Is Secret Code')
-        const user = await User.findOne({_id:decode._id,'tokens.token':token})
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decode = jwt.verify(token, 'this Is Secret Code');
+        const user = await User.findOne({ _id: decode._id, 'tokens.token': token });
 
-        if(!user){
-            throw new Error()
+        if (!user) {
+            throw new Error();
         }
 
-        req.user = user
-        next()
-    }catch (e) {
-        res.status(401).send( {error: 'please take auth'} )
+        // Attach user info including role to request
+        req.user = {
+            _id: user._id,
+            role: decode.role
+        };
+        next();
+    } catch (e) {
+        res.status(401).send({ error: 'please authenticate' });
     }
-}
+};
 
-module.exports = auth
+module.exports = auth;
