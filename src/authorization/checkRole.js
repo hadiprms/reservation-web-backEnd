6e6b3146d28c35518d12789c7909254bf3f480a5
hadi.prms <hadi.pourmohammad1383@gmail.com>
2025-08-03 15:58:17 +0330

    exported checkMarketer function

--- src/authorization/checkRole.js
+++ src/authorization/checkRole.js
@@ -34,21 +34,21 @@ const checkMarketer = async (req, res, next) => {
     }
 };
 
-const checkOwner = async (req , res, next) => {
-    try{
-        const userId = req.user._id;
-        const user = await User.findById(userId);
-        if (!user) {
-            return res.status(404).send({ error: 'User not found' });
-            // this code is surplus check it later, our error came form please auth, this is not working
-        }
-        if (user.role !== 'Owner') {
-            return res.status(403).send({ error: 'Access denied. Owner only.' });
-        }
-        next();
-    } catch (error) {
-        res.status(500).send({ error: 'Server error' });
-    }
-}
+// const checkOwner = async (req , res, next) => {
+//     try{
+//         const userId = req.user._id;
+//         const user = await User.findById(userId);
+//         if (!user) {
+//             return res.status(404).send({ error: 'User not found' });
+//             // this code is surplus check it later, our error came form please auth, this is not working
+//         }
+//         if (user.role !== 'Owner') {
+//             return res.status(403).send({ error: 'Access denied. Owner only.' });
+//         }
+//         next();
+//     } catch (error) {
+//         res.status(500).send({ error: 'Server error' });
+//     }
+// }
 
-module.exports = checkOwner , checkAdmin;
+module.exports =  checkAdmin , checkMarketer;
