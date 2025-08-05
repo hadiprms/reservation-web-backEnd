6847bbf6e1fe4bb7a2460d03cf9c4e73f02e2b4e
hadi.prms <hadi.pourmohammad1383@gmail.com>
2025-08-05 12:52:13 +0330

    updated checkRole function to accept more than one role

--- src/authorization/checkRole.js
+++ src/authorization/checkRole.js
@@ -1,17 +1,23 @@
 const User = require('../models/userSchema');
 
-const checkRole = (role) => {
+const checkRole = (roles) => {
+  // roles should be an array of allowed roles
   return async (req, res, next) => {
     try {
       const userId = req.user._id;
       const user = await User.findById(userId);
+      
       if (!user) {
         return res.status(404).send({ error: 'User not found' });
       }
-      if (user.role !== role) {
-        return res.status(403).send({ error: `Access denied. ${role} role required.` });
+      
+      // Check if user role is in the permissible roles array
+      if (!roles.includes(user.role)) {
+        return res.status(403).send({ error: `Access denied. Requires one of: ${roles.join(', ')}` });
       }
+      
       next();
+      
     } catch (error) {
       res.status(500).send({ error: 'Server error' });
     }
