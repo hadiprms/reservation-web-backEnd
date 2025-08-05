24d579e278cb783bda4f628f0774950520c4352b
hadi.prms <hadi.pourmohammad1383@gmail.com>
2025-08-05 12:33:07 +0330

    updated logic of checking role, will pass argument in routes

--- src/authorization/checkRole.js
+++ src/authorization/checkRole.js
@@ -1,54 +1,22 @@
 const User = require('../models/userSchema');
 
-const checkAdmin = async (req, res, next) => {
+const checkRole = (role) => {
+  return async (req, res, next) => {
     try {
-        const userId = req.user._id;
-        const user = await User.findById(userId);
-        if (!user) {
-            return res.status(404).send({ error: 'User not found' });
-            // this code is surplus check it later, our error came form please auth, this is not working
-        }
-        if (user.role !== 'Admin') {
-            return res.status(403).send({ error: 'Access denied. Admins only.' });
-        }
-        next();
+      const userId = req.user._id;
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).send({ error: 'User not found' });
+      }
+      if (user.role !== role) {
+        return res.status(403).send({ error: `Access denied. ${role} role required.` });
+      }
+      next();
     } catch (error) {
-        res.status(500).send({ error: 'Server error' });
+      res.status(500).send({ error: 'Server error' });
     }
+  };
 };
 
-const checkMarketer = async (req, res, next) => {
-    try {
-        const userId = req.user._id;
-        const user = await User.findById(userId);
-        if (!user) {
-            return res.status(404).send({ error: 'User not found' });
-            // this code is surplus check it later, our error came form please auth, this is not working
-        }
-        if (user.role !== 'Marketer') {
-            return res.status(403).send({ error: 'Access denied. Marketer only.' });
-        }
-        next();
-    } catch (error) {
-        res.status(500).send({ error: 'Server error' });
-    }
-};
 
-// const checkOwner = async (req , res, next) => {
-//     try{
-//         const userId = req.user._id;
-//         const user = await User.findById(userId);
-//         if (!user) {
-//             return res.status(404).send({ error: 'User not found' });
-//             // this code is surplus check it later, our error came form please auth, this is not working
-//         }
-//         if (user.role !== 'Owner') {
-//             return res.status(403).send({ error: 'Access denied. Owner only.' });
-//         }
-//         next();
-//     } catch (error) {
-//         res.status(500).send({ error: 'Server error' });
-//     }
-// }
-
-module.exports =  {checkAdmin , checkMarketer};
+module.exports =  { checkRole };
