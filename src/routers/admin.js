61ebb36ffcd3d2da3a2df5a85b1e32302f510e4f
hadi.prms <hadi.pourmohammad1383@gmail.com>
2025-08-07 12:07:06 +0330

    move approve role request route to roleRequest.js

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -122,44 +122,4 @@ router.get('/admin/role-requests', async (req, res) => {
   }
 });
 
-router.patch('/admin/approve-role-request/:id', auth, checkRole([roles.value.Admin, roles.value.SuperAdmin]), async (req, res) => {
-  const roleRequestId = req.params.id;
-
-  try {
-    // Find the RoleRequest document by its ID
-    const roleRequestDoc = await RoleRequest.findById(roleRequestId);
-
-    if (!roleRequestDoc || roleRequestDoc.status !== 'Pending') {
-      return res.status(404).send({ error: 'Role request not found or already processed' });
-    }
-
-    const userId = roleRequestDoc.userId;
-    const roleToAssign = roleRequestDoc.roleRequest;
-
-    // Find the user using userId
-    const user = await User.findById(userId);
-
-    if (!user || user.deletedAt) {
-      return res.status(404).send({ error: 'User not found' });
-    }
-
-    // Update user's role and reset roleRequest
-    if (!user.role.includes(roleToAssign)) {
-      user.role.push(roleToAssign);
-    }
-    user.roleRequest = null;
-    await user.save();
-
-    // Update the RoleRequest document's status to Approved
-    roleRequestDoc.status = 'Approved';
-    roleRequestDoc.processedAt = new Date();
-    await roleRequestDoc.save();
-
-    res.send({ message: `User's role updated to ${roleToAssign} based on role request.` });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send({ error: 'Failed to process role request' });
-  }
-});
-
 module.exports = router;
