ac365b029d7580fafef0482631a92a130b4b3001
hadi.prms <hadi.pourmohammad1383@gmail.com>
2025-08-11 15:00:32 +0330

    update database name

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/test').then(() => {
+mongoose.connect('mongodb://127.0.0.1:27017/data').then(() => {
     console.log('Connected to MongoDB');
 }).catch((error) => {
     console.error('Error connecting to MongoDB:', error);
