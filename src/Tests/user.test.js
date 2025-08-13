d254bae3f64f211d0f9d2e331bf736b5ef41876a
hadi.prms <hadi.pourmohammad1383@gmail.com>
2025-08-13 15:18:58 +0330

    update to delete all past data's from database and new one replace

--- src/Tests/user.test.js
+++ src/Tests/user.test.js
@@ -1,20 +1,22 @@
 const request = require('supertest')
 const app = require('../app')
 const User = require('../models/userSchema')
-const Test = require('supertest/lib/test')
 
 const userOne = {
-    name:'Hadi',
-    email:'test@gmail.com',
-    password:'testpass'
+    email: "test456@gmail.com",
+    lastName: "sflst",
+    firstName: "ffirst",
+    password: "ddd32sssss",
+    role: "Marketer"
 }
 
-test('Should sign up new user' , async() => {
-    await request(app).post('/signup').send({
-    "email": "test46@gmail.com",
-    "lastName": "sflst",
-    "firstName":"ffirst",
-    "password":"ddd32sssss",
-    "role":"Marketer"
-    })
+beforeEach(async()=>{
+    await User.deleteMany()
+    await new User(userOne).save()
+})
+
+test('Should sign up new user', async () => {
+    await request(app)
+        .post('/signup')
+        .send(userOne)
 })
