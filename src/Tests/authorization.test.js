44bfc9ed3e8723c7412270f545aed8aaf0831c7d
hadi.prms <hadi.pourmohammad1383@gmail.com>
2025-08-13 15:57:48 +0330

    add jwt to user

--- src/Tests/authorization.test.js
+++ src/Tests/authorization.test.js
@@ -1,15 +1,22 @@
+require('dotenv').config({ path: __dirname + '/../test.env' });
 const request = require('supertest')
 const app = require('../app')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const User = require('../models/userSchema')
 
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
     email: "test456@gmail.com",
     lastName: "sflst",
     firstName: "ffirst",
     password: "ddd32sssss",
-    role: "Marketer"
+    role: "Marketer",
+    tokens:[{
+        token: jwt.sign({ _id: userOneId }, process.env.JWTSecretCode)
+    }]
 }
-
+console.log(userOne.tokens)
 beforeEach(async()=>{
     await User.deleteMany()
     await new User(userOne).save()
@@ -36,4 +43,14 @@ test('should login exisiting user', async () => {
             password:userOne.password
         })
         .expect(200)
+})
+
+test('should login none-exisiting user', async () => {
+    await request(app)
+        .post('/login')
+        .send({
+            email:userOne.email,
+            password:'wrongPassword'
+        })
+        .expect(400)
 })
