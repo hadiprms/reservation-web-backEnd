a99f37828f8b0e0cedfd8f3f71168f5dcf237dc2
hadi.prms <hadi.pourmohammad1383@gmail.com>
2025-08-20 15:35:06 +0330

    added swagger docs route

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const hotelRouter = require('./routers/hotel');
 const authorizationRouter = require('./routers/authorization');
 const adminRouter = require('./routers/admin');
 const roleRequestRouter = require('./routers/roleRequest');
+const { swaggerUi, swaggerSpec } = require('../swagger');
 
 const app = express();
 app.use(express.json());
@@ -18,5 +19,8 @@ app.use(hotelRouter);
 app.use(authorizationRouter);
 app.use(adminRouter);
 app.use(roleRequestRouter);
+
+// Swagger Docs
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = app;
