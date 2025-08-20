269f97d471edda1fbd2af052a5b1433303032bbb
hadi.prms <hadi.pourmohammad1383@gmail.com>
2025-08-20 17:19:32 +0330

    noticed have bug in auth , fixed it ( if had problem again look here but problem was in previous codes)

--- src/authorization/authorization.js
+++ src/authorization/authorization.js
@@ -1,21 +1,22 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userSchema');
 
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+const JWTToken = process.env.JWTSecretCode;
+
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '');
-        const decode = jwt.verify(token, 'this Is Secret Code');
+        const decode = jwt.verify(token, JWTToken);
         const user = await User.findOne({ _id: decode._id, 'tokens.token': token });
 
         if (!user) {
             throw new Error();
         }
 
-        // Attach user info including role to request
-        req.user = {
-            _id: user._id,
-            role: decode.role
-        };
+        req.token = token;      // Attach token for logout
+        req.user = user;        // Attach full user document
         next();
     } catch (e) {
         res.status(401).send({ error: 'please authenticate' });
