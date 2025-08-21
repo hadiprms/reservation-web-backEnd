426596cd2d27af56e573950e712e1d14be43c26d
hadi.prms <hadi.pourmohammad1383@gmail.com>
2025-08-21 12:57:32 +0330

    added swagger to hotel.js route's

--- src/routers/hotel.js
+++ src/routers/hotel.js
@@ -7,6 +7,53 @@ const roles = require('../models/roles');
 
 const router = express.Router()
 
+/**
+ * @swagger
+ * /hotel:
+ *   post:
+ *     summary: Post a hotel
+ *     tags: [Hotel]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - roles
+ *             properties:
+ *                 hotelName:
+ *                   type: string
+ *                   example: Sky
+ *                 adress:
+ *                   type: string
+ *                   example: TBZ
+ *                 pointOfUsers:
+ *                   type: number
+ *                   example: 3
+ *                 description:
+ *                   type: string
+ *                   example: lorem
+ *                 price:
+ *                   type: number
+ *                   example: 10.3
+ *                 bookTime:
+ *                   type: number
+ *                   example: 2025-07-30
+ *                 exitTime:
+ *                   type: string
+ *                   example: 2025-07-31
+ *     responses:
+ *       201:
+ *         description: Successfully Posted hotel
+ *       400:
+ *         description: Hotel with the same data already exists.
+ *       500:
+ *         description: Server error
+ */
+
 router.post('/hotel', auth , checkRole([roles.value.Admin, roles.value.Marketer, roles.value.SuperAdmin]), async (req, res) => {
     try {
         const existingHotel = await Hotel.findOne(req.body);
@@ -21,6 +68,29 @@ router.post('/hotel', auth , checkRole([roles.value.Admin, roles.value.Marketer,
         res.status(400).send(error);
     }
 });
+
+
+/**
+ * @swagger
+ * /reserve-hotel/{hotelId}:
+ *   post:
+ *     summary: Reserve a hotel
+ *     tags: [Hotel]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: hotelId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Hotel ID to reserve
+ *     responses:
+ *       200:
+ *         description: Successfully booked hotel
+ *       500:
+ *         description: Error reserving tour
+ */
 
 router.post('/reserve-hotel/:hotelId', auth , async (req, res) => {
     const userId = req.user._id;
@@ -40,6 +110,22 @@ router.post('/reserve-hotel/:hotelId', auth , async (req, res) => {
         res.status(500).send({ error: 'Error reserving tour' });
     }
 });
+
+
+/**
+ * @swagger
+ * /hotels:
+ *   get:
+ *     summary: Get's all hotels
+ *     tags: [Hotel]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: All hotel's found
+ *       500:
+ *         description: Server error
+ */
 
 router.get('/hotels' , async (req , res) =>{
     try {
