1e2a57feb5bad2e7117d96e16c72f10be30fa9de
hadi.prms <hadi.pourmohammad1383@gmail.com>
2025-08-26 22:38:07 +0330

    handle cors error for front-end

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('./db/mongoose'); // DB connection
+const cors = require('cors');
 
 const userRouter = require('./routers/user');
 const tourRouter = require('./routers/tour');
@@ -11,6 +12,13 @@ const roleRequestRouter = require('./routers/roleRequest');
 const { swaggerUi, swaggerSpec } = require('../swagger');
 
 const app = express();
+
+// Enable CORS for frontend origin
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Serve uploaded images
