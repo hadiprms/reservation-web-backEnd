2981fe6affd7912334affdeeea906b29f1c2ac8b
hadi.prms <hadi.pourmohammad1383@gmail.com>
2025-09-10 17:51:13 +0330

    update hotelReservation property in user schema

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -62,17 +62,26 @@ const UserSchema = new mongoose.Schema({
             default: Date.now
         }
     }],
-    hotelReservations: [{
-        hotelId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Hotel',
-            required: true
-        },
-        reservationDate: {
-            type: Date,
-            default: Date.now
-        }
-    }],
+    hotelReservations: [
+        {
+            hotelId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Hotel',
+                required: true
+            },
+            bookTime: {
+                type: Date,
+                required: true
+            },
+            exitTime: {
+                type: Date,
+                required: true
+            },
+            reservationDate: {
+                type: Date,
+                default: Date.now
+            }
+        }],
     deletedAt:{
         type: Date,
         default: null
